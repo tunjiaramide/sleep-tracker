39b71448395f1f577e2dcdaeb5736923d34d13b6
Adetunji Aramide <adetunjiaramide@gmail.com>
2025-07-21 01:32:49 +0100

    added about, contact, footer, guest homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-export default function HomePage() {
+import Guest from "@/components/Guest";
+import { currentUser  } from "@clerk/nextjs/server";
+
+export default async function HomePage() {
+  const user = await currentUser();
+  if(!user) {
+    return <Guest />
+  }
+
   return (
-    <div>
-      <h2 className="text-2xl text-red-500">HomePage</h2>
-    </div>
+    <>
+      <h2>Welcome to our dashboard</h2>
+    </>
   );
 }
