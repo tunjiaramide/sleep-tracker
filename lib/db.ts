8be3ea7e858559719717b6102228bc5082e48124
Adetunji Aramide <adetunjiaramide@gmail.com>
2025-07-21 02:56:55 +0100

    adjusted build settings

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
+// add no-var
+
+/* eslint-disable no-var */
+
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
+/* eslint-disable no-var */
 export const db = globalThis.prisma || new PrismaClient();
 
 if(process.env.NODE_ENV !== 'production'){
